cbdb75cf33c422f93d1f8c58edb18ac7fb57896d
Wilson Faustino <wilson.rfaustino@gmail.com>
2020-10-18 23:14:03 -0300

    finalizada aula 1

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,1 +1,18 @@
-import {} from 'react-router-dom'
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Landing from './pages/Landing'
+import OrphanagesMap from './pages/OrphanagesMap'
+
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Landing} exact />
+        <Route path="/app" component={OrphanagesMap} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default Routes
